239ed06add74b6970a0e8863a0b6a1365eedbf15
Long Hoang <long@mindworker.de>
2015-09-13 11:17:25 +0800

    Modal for form and mail service

--- js/script.js
+++ js/script.js
@@ -11,6 +11,24 @@ function equalHeight(group) {
 
 $(document).ready(function() {   
     equalHeight($(".storybox"));
+
+    $(document).ready(function () {
+	$("input#submit").click(function(){
+            $.ajax({
+		type: "POST",
+		url: "process.php", //process to mail
+		data: $('form.contact').serialize(),
+		success: function(msg){
+                    $("#thanks").html(msg) //hide button and show thank you
+                    $("#form-content").modal('hide'); //hide popup  
+		},
+		error: function(){
+                    alert("failure");
+		}
+            });
+	});
+    });
+
 });
 
 function tooltipStory() {
